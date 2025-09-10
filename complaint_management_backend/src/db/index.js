a3e8460a81804bc8bb86e17154138265a7a89c88
ram <prudhviram2000@gmail.com>
2025-09-10 13:40:09 -0400

    added postgres aws

--- complaint_management_backend/src/db/index.js
+++ complaint_management_backend/src/db/index.js
@@ -9,6 +9,9 @@ const pool = new Pool({
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false // This is necessary for some cloud providers
+  }
 });
 
 // We export a query function that will be used by our controllers
